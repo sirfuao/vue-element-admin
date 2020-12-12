97ea74eaa9a5158ac01ff185058ad42d65dcb033
sirfuao <664431710@qq.com>
2020-12-12 18:23:44 +0800

    添加粒子背景

---
+++ vue_element_admin/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- 引入外部音乐 -->
+    <audio id="audio" autoplay="autoplay" loop="loop">
+      <source src="./assets/audios/music.mp3" type="audio/mpeg" />
+    </audio>
+    <!-- 路由占位符 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted() {
+    this.autoPlay()
+  },
+  methods: {
+    autoPlay() {
+      var bodyObj = document.getElementsByTagName('body')[0]
+      var audio = document.getElementById('audio')
+      console.log(bodyObj)
+      bodyObj.onclick = function () {
+        console.log('123')
+        console.log(audio.paused)
+         audio.play() // 播放
+
+        // if (audio.paused) {
+        //   // audio.paused  可以用来判断当前是否为暂停状态，true 就是暂停， false为播放状态
+        //   audio.play() // 播放
+        // } else {
+        //   //  audio.pause();  暂停
+        //   audio.play()
+        // }
+      }
+    },
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ vue_element_admin/src/components/Home.vue
@@ -0,0 +1,168 @@
+<template>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/sirfuao.jpg" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <div class="btn">
+        <el-button style="margin-right:12px;" type="info" @click="logout">退出</el-button>
+        <el-dropdown @command="handleCommand">
+          <el-button type="primary"> 更多<i class="el-icon-arrow-down el-icon--right"></i> </el-button>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="github">GitHub</el-dropdown-item>
+            <el-dropdown-item command="csdn">CSDN博客</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao',
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: '',
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 跳转到我的GitHub
+    handleCommand(command){
+      // command 是下拉菜单传递过来的参数;
+      if(command=='github'){
+        window.location.href = 'http://github.com/sirfuao';
+      }else if(command=="csdn") {
+        window.location.href = 'https://blog.csdn.net/qq_43353619';
+      } 
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮，切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  // padding: 15px 0;
+  height: 80px !important;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+    margin: 15px 0px 15px 15px;
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
